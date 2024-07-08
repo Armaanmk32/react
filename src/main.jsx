2baacb1d47d154fc3e2ab3c3004b92ba3b4b4a88
Armaanmk32 <abdirahmanabdallanasser@gmail.com>
2024-07-08 11:27:15 +0300

    waxaan sameyney nal

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./style.css"
+import Header from './components/Header'
+import Light from './components/Light'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+<div>
+<Header />
+<Light />
+
+
+  </div>
+
+)
